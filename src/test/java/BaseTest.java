e7ceff26e35f0a145552c686683dbb6bd71b8919
GigiMamaladze <gmamaladze@solvd.com>
2024-03-13 18:12:58 +0400

    Improved code

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,6 +5,10 @@ public abstract class BaseTest {
 
     protected final String BEARER_TOKEN = "Bearer 17a915d98bc061595c53aa898006e9e63a8d5935bfe8d316b0399ebbd31af775";
 
+    protected final String CONTENT_TYPE = "application/json";
+
+    protected final String CONNECTION = "keep-alive";
+
     public User generateRandomUser() {
         User user = new User();
         user.setName(RandomStringUtils.randomAlphabetic(5));
--- src/test/java/GraphQLTests.java
+++ src/test/java/GraphQLTests.java
@@ -5,20 +5,19 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import services.GraphQLService;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
 import static io.restassured.RestAssured.given;
 
 public class GraphQLTests extends BaseTest {
 
     private final List<Long> userIds = new ArrayList<>();
-
     private final String ENDPOINT = "https://gorest.co.in/public/v2/graphql";
-
     private final GraphQLService graphQLService = new GraphQLService(BEARER_TOKEN, ENDPOINT);
-
+    private final String REQUEST_BODY_DIRECTORY = "src/test/resources/graphql/";
 
     @AfterMethod(groups = "created", alwaysRun = true)
     public void deleteCreatedUser() {
@@ -26,145 +25,113 @@ public class GraphQLTests extends BaseTest {
         userIds.clear();
     }
 
+    private String readRequestBodyFromFile(String fileName) throws IOException {
+        String filePath = REQUEST_BODY_DIRECTORY + fileName;
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    private Response sendRequestWithBody(String requestBody) {
+        return given()
+                .header("Authorization", BEARER_TOKEN)
+                .header("Content-Type", CONTENT_TYPE)
+                .header("Connection", CONNECTION)
+                .body(requestBody)
+                .when()
+                .post(ENDPOINT);
+    }
+
+    private String replaceDynamicParameters(String requestBody, User user) {
+        return requestBody.replace("{{userId}}", String.valueOf(user.getId()))
+                .replace("{{userName}}", user.getName())
+                .replace("{{userEmail}}", user.getEmail())
+                .replace("{{userGender}}", user.getGender())
+                .replace("{{userStatus}}", user.getStatus());
+    }
+
     // =============================== Tests ==================================================
 
     @Test
-    public void getUsersQueryTest() {
-        Response response = given()
-                .header("Authorization", BEARER_TOKEN)
-                .header("Content-Type", "application/json")
-                .header("Connection", "keep-alive")
-                .body("{\n" +
-                        "  \"query\": \"query Users { users { nodes { id email gender name status } }}\"\n" +
-                        "}")
-                .when()
-                .post(ENDPOINT);
-
+    public void getUsersQueryTest() throws IOException {
+        String requestBody = readRequestBodyFromFile("get_users_query.json");
+        Response response = sendRequestWithBody(requestBody);
         response.prettyPrint();
         response.then().statusCode(200);
-
         List<User> userList = response.jsonPath().getList("data.users.nodes", User.class);
-
         Assert.assertFalse(userList.isEmpty(), "User list should not be empty !");
     }
 
-
     @Test(groups = "created")
-    public void getUserByIdQueryTest() {
+    public void getUserByIdQueryTest() throws IOException {
         User user = generateRandomUser();
         user = graphQLService.createUser(user);
         userIds.add(user.getId());
-
-        Response response = given()
-                .header("Authorization", BEARER_TOKEN)
-                .header("Content-Type", "application/json")
-                .header("Connection", "keep-alive")
-                .when()
-                .body("{\n" +
-                        "  \"query\": \"query User { user(id: \\\""+ user.getId() +"\\\") { id email gender name status }}\"\n" +
-                        "}")
-                .post(ENDPOINT);
-
-        User getUser = response.jsonPath().getObject("data.user", User.class);;
-
+        String requestBody = readRequestBodyFromFile("get_user_by_id_query.json");
+        requestBody = replaceDynamicParameters(requestBody, user);
+        Response response = sendRequestWithBody(requestBody);
+        User getUser = response.jsonPath().getObject("data.user", User.class);
         response.prettyPrint();
         response.then().statusCode(200);
-
-        Assert.assertEquals(getUser, user, "User is not as excepted !");
+        Assert.assertEquals(getUser, user, "User is not as expected !");
     }
 
     @Test(groups = "created")
-    public void createUserMutationTest() {
+    public void createUserMutationTest() throws IOException {
         User user = new User();
         user.setName(RandomStringUtils.randomAlphabetic(5));
         user.setEmail(RandomStringUtils.randomAlphabetic(6) + "@Gmgmail.com");
         user.setGender("male");
         user.setStatus("inactive");
-        Response response = given()
-                .header("Authorization", BEARER_TOKEN)
-                .header("Content-Type", "application/json")
-                .header("Connection", "keep-alive")
-                .body("{\n" +
-                        "  \"query\": \"mutation CreateUser { createUser( input: { name: \\\"" + user.getName() + "\\\", email: \\\"" + user.getEmail() + "\\\", " +
-                        "gender: \\\"" + user.getGender() + "\\\", status: \\\"" + user.getStatus() + "\\\" } ) { user { id email gender name status } }}\"\n" +
-                        "}")
-                .when()
-                .post(ENDPOINT);
-
+        String requestBody = readRequestBodyFromFile("create_user_mutation.json");
+        requestBody = replaceDynamicParameters(requestBody, user);
+        Response response = sendRequestWithBody(requestBody);
         response.prettyPrint();
         response.then().statusCode(200);
-
         user = response.jsonPath().getObject("data.createUser.user", User.class);
         User createdUser = graphQLService.getUser(user.getId());
         userIds.add(user.getId());
-
         Assert.assertEquals(createdUser, user, "User is not created !");
     }
 
     @Test(groups = "created")
-    public void updateUserTest() {
+    public void updateUserTest() throws IOException {
         User user = generateRandomUser();
         user = graphQLService.createUser(user);
         userIds.add(user.getId());
-
         String initialUserName = user.getName();
-
         User updatedUser = user;
         updatedUser.setName("UpdatedName");
-
-        Response putResponse = given()
-                .header("Authorization", BEARER_TOKEN)
-                .header("Content-Type", "application/json")
-                .header("Connection", "keep-alive")
-                .when()
-                .body("{\n" +
-                        "  \"query\": \"mutation UpdateUser { updateUser(input: { name: \\\""+ updatedUser.getName() + "\\\", " +
-                        "id: \\"+ user.getId() + "\\ }) " +
-                        "{ user { email gender id name status } }}\"\n" +
-                        "}")
-                .post(ENDPOINT);
-
+        String requestBody = readRequestBodyFromFile("update_user_mutation.json");
+        requestBody = replaceDynamicParameters(requestBody, updatedUser);
+        Response putResponse = sendRequestWithBody(requestBody);
         putResponse.prettyPrint();
         putResponse.then().statusCode(200);
-
         User getUser = graphQLService.getUser(updatedUser.getId());
-
-        Assert.assertNotEquals(getUser.getName(), initialUserName, "User name should not be equal of initial created user name !");
+        Assert.assertNotEquals(getUser.getName(), initialUserName, "User name should not be equal to initial created user name !");
         Assert.assertEquals(getUser, updatedUser, "User is not updated !");
     }
 
-
     @Test
-    public void deleteUserMutationTest() {
+    public void deleteUserMutationTest() throws IOException {
         User user = generateRandomUser();
         user = graphQLService.createUser(user);
-
         Response response = given()
                 .header("Authorization", BEARER_TOKEN)
-                .header("Content-Type", "application/json")
-                .header("Connection", "keep-alive")
-                .body("{\n" +
-                        "  \"query\": \"mutation DeleteUser { deleteUser(input: { id: \\" + user.getId() + "\\ }) { user { id email gender name status } }}\"\n" +
-                        "}")
+                .header("Content-Type", CONTENT_TYPE)
+                .header("Connection", CONNECTION)
+                .body(readRequestBodyFromFile("delete_user_mutation.json"))
                 .when()
                 .post(ENDPOINT);
-
         response.prettyPrint();
         response.then().statusCode(200);
-
         Response getResponse = given()
                 .header("Authorization", BEARER_TOKEN)
-                .header("Content-Type", "application/json")
-                .header("Connection", "keep-alive")
+                .header("Content-Type", CONTENT_TYPE)
+                .header("Connection", CONNECTION)
+                .body(readRequestBodyFromFile("get_user_by_id_query.json"))
                 .when()
-                .body("{\n" +
-                        "  \"query\": \"query User { user(id: \\"+ user.getId() +"\\) { email gender id name status }}\"\n" +
-                        "}")
                 .post(ENDPOINT);
-
         getResponse.prettyPrint();
         String errorMessage = getResponse.jsonPath().getString("errors[0].message");
-
         // Verifying the error message
         Assert.assertEquals(errorMessage, "Resource not found!", "Error message mismatch");
     }
